4a96935b1fc2f9ddeb6acc1378074eb6fd076258
Dev_Breezy <nwabuikecharles@gmail.com>
2024-10-31 22:07:35 -0400

    added test cases

--- src/Solution.java
+++ src/Solution.java
@@ -2,8 +2,20 @@ import java.util.HashSet;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {10, 20, 30, 40, 50};
-        System.out.println(longestConsecutive(nums));
+        System.out.println("Test Case 1:");
+        System.out.println(longestConsecutive(new int[] {1, 2, 3, 4, 5}));
+        System.out.println("Test Case 2:");
+        System.out.println(longestConsecutive(new int[] {0}));
+        System.out.println("Test Case 3:");
+        System.out.println(longestConsecutive(new int[] {5, 5, 5, 5}));
+        System.out.println("Test Case 4:");
+        System.out.println(longestConsecutive(new int[] {0,-1, 9,-3,-2, 2, 1, 5}));
+        System.out.println("Test Case 5:");
+        System.out.println(longestConsecutive(new int[] {10, 20, 30, 40, 50}));
+        System.out.println("Test Case 6:");
+        System.out.println(longestConsecutive(new int[] {1, 9, 3, 10, 4, 20, 2}));
+        System.out.println("Test Case 7:");
+        System.out.println(longestConsecutive(new int[] {1, 3, 5, 2, 4, 6, 7, 9, 8, 12, 13, 10, 11}));
     }
 
     public static int longestConsecutive(int[] nums) {
